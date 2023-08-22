5100529d1823280f7fd1f409c377a94c7849f08f
MovieMix <officialmoviemix@gmail.com>
2023-08-23 02:22:34 +0530

    update

--- MoviesWebsiteGouranga/settings.py
+++ MoviesWebsiteGouranga/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=#qa__tj3b45k+j$^2^s9l0tt8s(epk^+mu(xe*xwv+x^#3vhm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['moviemix.pythonanywhere.com','127.0.0.1:8000','127.0.0.1']
 
