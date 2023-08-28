4c946f91cdccbfa0c9da8b2986b547ac48816910
MovieMix <officialmoviemix@gmail.com>
2023-08-29 00:03:26 +0530

    u

--- MoviesWebsiteGouranga/settings.py
+++ MoviesWebsiteGouranga/settings.py
@@ -119,11 +119,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT ='/home/moviemix/MoviesWebsiteGouranga/static'
