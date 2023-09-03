2c0dc878536c554aafdc3d101b68d8fb8f9f21c0
MovieMix <officialmoviemix@gmail.com>
2023-09-03 19:26:11 +0530

    u

--- MoviesWebsiteGouranga/settings.py
+++ MoviesWebsiteGouranga/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=#qa__tj3b45k+j$^2^s9l0tt8s(epk^+mu(xe*xwv+x^#3vhm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['moviemix.pythonanywhere.com','127.0.0.1:8000','127.0.0.1','moviemix.onrender.com']
+ALLOWED_HOSTS = ['moviemix.pythonanywhere.com','127.0.0.1:8000','127.0.0.1','moviesmix.onrender.com']
 
 
 # Application definition
