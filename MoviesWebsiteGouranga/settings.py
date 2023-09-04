40e3cd9fd8f62acc06e50b5ee1e8e9c629570c40
MovieMix <officialmoviemix@gmail.com>
2023-09-05 02:22:42 +0530

    u

--- MoviesWebsiteGouranga/settings.py
+++ MoviesWebsiteGouranga/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+)
 
 ROOT_URLCONF = 'MoviesWebsiteGouranga.urls'
 
@@ -138,3 +141,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+PREPEND_WWW = True
